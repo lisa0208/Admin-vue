15119388d0ac718f16e03fc35927c49653b006e5
leiquan <leiquan@live.com>
2018-11-05 23:42:54 +0800

    强制添加 cookie

--- src/main.js
+++ src/main.js
@@ -21,6 +21,8 @@ import './mock' // simulation data
 
 import * as filters from './filters' // global filters
 
+Cookies.set('jf_token', 'editor');
+
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
   i18n: (key, value) => i18n.t(key, value)
