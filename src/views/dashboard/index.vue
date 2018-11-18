40890d77a9f80c60bb61f57d5606359743fa705e
leiquan <leiquan@live.com>
2018-11-19 02:43:39 +0800

    提交

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,31 +1,93 @@
 <template>
   <div class="dashboard-container">
-    <component :is="currentRole"/>
+
+    <el-row :gutter="20" class='data-row'>
+
+      <el-col :span="12">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>今日 DAU</span>
+          </div>
+          <div class="text">
+            <el-progress type="circle" :percentage="100" :status="text">Done</el-progress>
+          </div>
+        </el-card>
+      </el-col>
+
+      <el-col :span="12">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>今日新增用户数</span>
+          </div>
+          <div class="text">
+            <el-progress type="circle" :percentage="100" :status="text">1000</el-progress>
+          </div>
+        </el-card>
+      </el-col>
+
+    </el-row>
+
+   <el-row :gutter="20">
+
+      <el-col :span="12">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>今日订单量</span>
+          </div>
+          <div class="text">
+            <el-progress type="circle" :percentage="100" status="text">1000</el-progress>
+          </div>
+        </el-card>
+      </el-col>
+
+      <el-col :span="12">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>今日订单总额</span>
+          </div>
+          <div class="text">
+            <el-progress type="circle" :percentage="100" status="text">1000</el-progress>
+          </div>
+        </el-card>
+      </el-col>
+
+    </el-row>
+
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import adminDashboard from './admin'
-import editorDashboard from './editor'
+import { mapGetters } from "vuex";
+import adminDashboard from "./admin";
+import editorDashboard from "./editor";
 
 export default {
-  name: 'Dashboard',
+  name: "Dashboard",
   components: { adminDashboard, editorDashboard },
   data() {
     return {
-      currentRole: 'adminDashboard'
-    }
+      currentRole: "adminDashboard"
+    };
   },
   computed: {
-    ...mapGetters([
-      'roles'
-    ])
+    ...mapGetters(["roles"])
   },
   created() {
-    if (!this.roles.includes('admin')) {
-      this.currentRole = 'editorDashboard'
+    if (!this.roles.includes("admin")) {
+      this.currentRole = "editorDashboard";
     }
   }
-}
+};
 </script>
+
+<style>
+.el-card__body {
+  text-align: center !important;
+}
+.dashboard-container {
+  padding: 20px;
+}
+.data-row {
+  margin-bottom: 20px;
+}
+</style>
